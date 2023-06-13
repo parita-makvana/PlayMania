a12aa31660afbee9dc45921e15253a82f913e3ef
Dipti Sharma <diptisharma@FVFXNB4TJ1WK.local>
2023-06-13 11:43:21 +0530

    login/register

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 // Here in this we can establish the DB connection
 const { Client } = require("pg");
-
+const dotenv = require("dotenv")
+dotenv.config();
 
 const client = new Client({
-host: "localhost",
-user: "postgres",
+host: process.env.HOST,
+user: process.env.USER,
 port: 5432,
-password: "@9829233939",
-database: "game_app",
+password: process.env.PASSWORD,
+database: process.env.DATABASE
 });
-
 
 module.exports = client;
